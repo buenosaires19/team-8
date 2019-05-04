055922c339e0d884c33f1115d349b19e903e6e41
deborahfarinas <deborahgfarinas@gmail.com>
2019-05-04 02:27:16 -0400

    Formcandidata terminado

--- front/containers/Main.jsx
+++ front/containers/Main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import FormPostulacion from '../components/formPostulacion';
+import FormCandidata from '../components/formCandidata';
 import Home from '../components/Home';
 
 class Main extends React.Component {
@@ -16,6 +17,7 @@ class Main extends React.Component {
                 {/* <BtnPost/> */}
                 {/* <Home/> */}
                 <Switch>
+                <Route exact path="/formCandidata" render={({history}) => (<FormCandidata history={history} />)} />
                     <Route exact path="/formRegister" render={({history}) => (<FormPostulacion history={history} />)} />
                     <Route exact path="/" render={() => (<Home />)} />
 
