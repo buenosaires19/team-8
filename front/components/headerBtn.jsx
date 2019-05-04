cf078383bc9fc433d1f1f29829d103f56d4b4e54
amatana <amatana95@gmail.com>
2019-05-04 05:02:56 -0300

    detalles Form Potulante

--- front/components/headerBtn.jsx
+++ front/components/headerBtn.jsx
@@ -6,8 +6,8 @@ export default () => (
         <div><h1 style={{
                 color:'#6102A3', 
                 fontFamily: '"Caveat Brush", cursive',
-                padding: '10px',
-                }}><strong>Grandes Mujeres</strong> - Este mes es de la Mujeres en Biología! </h1></div>
-        <div style={{textAlign:'right', paddingRight:'10px'}}><Link to='/formRegister'><button id='botonEstilos'><strong> POSTULA! </strong></button></Link></div>
+                padding: '25px',
+                }}><strong>Grandes Mujeres</strong> - Este mes es de las Mujeres en Biología! </h1></div>
+        <div style={{textAlign:'right', paddingRight:'10px'}}><Link to='/formRegister'><button id='botonEstilo'><strong> POSTULA! </strong></button></Link></div>
     </div>
 )
